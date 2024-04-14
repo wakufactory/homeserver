c848c6fac491b931834b698a54d82f7f95faed98
waku <waku@lang.co.jp>
2024-04-14 17:14:13 +0900

    initial

---
+++ config.js
@@ -0,0 +1,8 @@
+const Server = "https://192.168.23.4/"
+const Shareroot = "share/" 
+const Openlist = [
+	{reg:/\.(jpg|jpeg|png|gif)$/i,open:"apps/fullimg.html?"},
+	{reg:/\.(mov|mp4|m4v)$/i,open:"apps/video.html?"},
+	{reg:/\.(glb|vrm)$/i,open:"https://wakufactory.jp/wxr/tools/glbview.html?"},
+	{reg:/\.ply$/,open:"https://wakufactory.jp/wxr/splats/gsaf.html?"}
+]
---
+++ start.sh
@@ -0,0 +1,2 @@
+http-server . -p 443 -S -C local.crt  -K local.key --cors > log &
+
