a3ad74dcb2294cc757ebe91442b417c183fcca6f
Waku OE <waku@WakunoMacBook-Air.local>
2024-04-16 22:39:32 +0900

    add extension

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const Server = "https://192.168.23.4/"
 const Shareroot = "share/" 
 const Openlist = [
-	{reg:/\.(jpg|jpeg|png|gif)$/i,open:"apps/fullimg.html?"},
-	{reg:/\.(mov|mp4|m4v)$/i,open:"apps/video.html?"},
+	{reg:/\.(jpg|jpeg|png|gif|bmp)$/i,open:"apps/fullimg.html?"},
+	{reg:/\.(mov|mp4|m4v|webm)$/i,open:"apps/video.html?"},
 	{reg:/\.pdf$/i,open:"apps/pdf/?"},
 	{reg:/\.(glb|vrm)$/i,open:"https://wakufactory.jp/wxr/tools/glbview.html?"},
 	{reg:/\.ply$/,open:"https://wakufactory.jp/wxr/splats/gsaf.html?"}
