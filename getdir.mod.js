d998a4d82276705c714d64c086e13fe0171a320a
Waku OE <waku@WakunoMacBook-Air.local>
2024-04-18 22:43:12 +0900

    add slideshow

--- getdir.mod.js
+++ getdir.mod.js
@@ -1,6 +1,6 @@
 export {getdir}
 
-function getdir(path) {
+function getdir(path,filter=null) {
 	return new Promise((resolve,reject)=>{
 		fetch(path).then(resp=>{
 			resp.text().then(data=>{
@@ -9,7 +9,7 @@ function getdir(path) {
 					let ret = null
 					const en = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"].indexOf(m)
 					if(en>=0) ret = en+1 
-					if(m.match(/([0-9]*)月/)) ret = RegExp.$1+1 
+					if(m.match(/([0-9]*)月/)) ret = parseInt(RegExp.$1)+1 
 					return ("0"+ret.toString()).substr(-2)
 				}
 				let flag = false 
@@ -20,13 +20,15 @@ function getdir(path) {
 						if(!m[6].match(/^\./)) {
 						const doc = new DOMParser().parseFromString(m[6], 'text/html')
 						const dt = m[3].match(/([0-9]*)-(.*)-([0-9]*) ([0-9]*):([0-9]*)/)
+						const name = doc.documentElement.textContent
+						if(filter && !name.match(filter)) return
 						list.push({
-							ic:m[1].replace("icon-",""),
+							type:(m[1]=="icon-_blank")?"dir":"file",
 							mode:m[2],
 							date:`${dt[3]}-${month(dt[2])}-${dt[1]} ${dt[4]}:${dt[5]}`,
 							size:m[4],
 							path:m[5].substr(2) ,
-							name:doc.documentElement.textContent})
+							name:name})
 						}
 					}
 				})
