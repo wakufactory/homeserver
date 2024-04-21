318f39b45323834f0e5d8dd777f8c41f0ced6579
waku <waku@lang.co.jp>
2024-04-21 13:06:07 +0900

    support zip images

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const Server = "https://192.168.23.4/"
 const Shareroot = "share/" 
 const Openlist = [
-	{reg:/\.(jpg|jpeg|png|gif|bmp)$/i,open:"apps/fullimg.html?"},
+	{reg:/\.(jpg|jpeg|png|gif|bmp|zip|epub)$/i,open:"apps/fullimg.html?"},
 	{reg:/\.(mov|mp4|m4v|webm)$/i,open:"apps/video.html?"},
 	{reg:/\.pdf$/i,open:"apps/pdf/?"},
 	{reg:/\.(glb|vrm)$/i,open:"https://wakufactory.jp/wxr/tools/glbview.html?"},
